5514334a01016eb749b32977f326a471c8414244
GMP Sankalpa <malithapiumal02@gmail.com>
2024-04-07 17:25:38 +0530

    first commit

---
+++ js/languages.js
@@ -0,0 +1,20 @@
+const languages = [
+  {
+    no: "0",
+    name: "Auto",
+    native: "Detect",
+    code: "auto",
+  },
+  {
+    no: "16",
+    name: "English",
+    native: "English",
+    code: "en",
+  },
+  {
+    no: "16",
+    name: "Sinhala",
+    native: "සිංහල",
+    code: "si",
+  },
+];
